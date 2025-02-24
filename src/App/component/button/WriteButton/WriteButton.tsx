8382064fc2e37d1b0f292f5ae1034d3ad4a2e53d
jieunsse <jieunsse@gmail.com>
2025-02-24 21:29:00 +0900

    feat : 게시판 페이지 UI 완성

--- src/App/component/button/WriteButton/WriteButton.tsx
+++ src/App/component/button/WriteButton/WriteButton.tsx
@@ -1,7 +1,15 @@
+import { useNavigate } from 'react-router-dom';
+
 interface writeButtonProps {
   className?: string;
 }
 
 export const WriteButton = ({ className }: writeButtonProps) => {
-  return <button className={className}>글쓰기</button>;
+  const navigate = useNavigate();
+
+  return (
+    <button className={className} onClick={() => navigate('/write')}>
+      글쓰기
+    </button>
+  );
 };
--- src/App/component/footer/Footer.tsx
+++ src/App/component/footer/Footer.tsx
@@ -1,21 +1,28 @@
 import style from './footer.module.css';
-import titleStyle from '@components/title/footerTitle/footerTitle.module.css';
 import { FooterTitle } from '@components/title/footerTitle/FooterTitle.tsx';
 import rocketLogo from 'src/shared/assets/img/logo/rocketLogo.svg';
 
-export const Footer = () => {
+interface FooterProps {
+  className: string;
+  title01: string;
+  title02: string;
+  title03: string;
+  title04: string;
+}
+
+export const Footer = ({
+  className,
+  title01,
+  title02,
+  title03,
+  title04,
+}: FooterProps) => {
   return (
-    <div className={style.footer}>
-      <FooterTitle title={'공지사항'} className={titleStyle.footerTitle01} />
-      <FooterTitle
-        title={'개인정보 처리방침'}
-        className={titleStyle.footerTitle02}
-      />
-      <FooterTitle title={'문의하기'} className={titleStyle.footerTitle03} />
-      <FooterTitle
-        title={'©Gostock. All reserved.'}
-        className={titleStyle.footerTitle04}
-      />
+    <div className={className}>
+      <FooterTitle title={'공지사항'} className={title01} />
+      <FooterTitle title={'개인정보 처리방침'} className={title02} />
+      <FooterTitle title={'문의하기'} className={title03} />
+      <FooterTitle title={'©Gostock. All reserved.'} className={title04} />
       <img src={rocketLogo} alt={'logo'} className={style.logo} />
     </div>
   );
